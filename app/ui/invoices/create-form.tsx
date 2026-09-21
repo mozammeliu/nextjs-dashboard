6f6b802a25ec5ff276edcbd0eb96e41665f9e265
Mozammel Haque <mozammeliu@gmail.com>
2026-09-22 01:16:01 +0600

    fixed 'app/ui/invoices/create-form.tsx' that was showing error while building in production

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -12,7 +12,9 @@ import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form action={createInvoice}>
+
+    //action={async (formData) => { await createInvoice(formData); }}
+    <form /*action={createInvoice}*/ action={async (formData) => { await createInvoice(formData); }}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
