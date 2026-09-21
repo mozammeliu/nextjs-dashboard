cdf49c4e991d8bfd485c1a61e94130bb7f0edc1b
Mozammel Haque <mozammeliu@gmail.com>
2026-09-22 02:57:39 +0600

    chapter-7 done

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -12,9 +12,7 @@ import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-
-    //action={async (formData) => { await createInvoice(formData); }}
-    <form /*action={createInvoice}*/ action={async (formData) => { await createInvoice(formData); }}>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
